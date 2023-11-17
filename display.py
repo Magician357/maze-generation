46e7541686f7f752ccc351f786b5c5bd308bd632
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-11-17 07:43:23 -0800

    add maze solving algorithm

--- display.py
+++ display.py
@@ -1,12 +1,54 @@
 import pygame
 from growingtree import generate_growing_tree_maze
-from random import randint
+from random import randint, shuffle
 
 def choose_index(length):
-    return length-1 if randint(1,2) == 2 else 0 if randint(1,2) == 1 else randint(0,length-1)
+    return length-1 if randint(1,2) == 2 else (0 if randint(1,2) == 1 else randint(0,length-1))
+
+# north, south, east, west
+
+opposite={1:2,2:1,3:4,4:3}
+
+def generate_directions(maze,start_point,end_point):
+    directions=[1,2,3,4]
+    dx=[0,0,0,1,-1]
+    dy=[0,-1,1,0,0]
+    cx,cy=start_point
+    shuffle(directions)
+    for direction in directions:
+        nx=cx+dx[direction]
+        ny=cy+dy[direction]
+        if nx >= 0 and ny >= 0 and nx < maze_width and ny < maze_height and directions_list[ny][nx] == 0:
+            # if (nx,ny) == end_point:
+            #     return
+            if maze[cx,cy][direction] == 0:
+                directions_list[ny][nx]=opposite[direction]
+                generate_directions(maze,(nx,ny),end_point)
+
+    dir_string=[" ","N","S","E","W","#"]
+    for y in range(maze_height):
+        for x in range(maze_width):
+            maze[x,y][5]=dir_string[directions_list[y][x]]
+
+def generate_path_from(cx,cy,curpath,sx,sy):
+    
+    if not sx or not sy:
+        sx,sy=cx,cy
+
+    print(cx,cy)
+    direction=directions_list[int(cy)][int(cx)]
+    if direction == 5:
+        return [(sx,sy)]+curpath
+    else:
+        dx=[0,0,0,1,-1]
+        dy=[0,-1,1,0,0]
+        nx=cx+dx[direction]
+        ny=cy+dy[direction]
+        curpath.append((nx,ny))
+        return generate_path_from(nx,ny,curpath,sx,sy)
 
 pygame.font.init()
-# print(pygame.font.get_fonts())
+# print(pygame.font.get_fonts()
 my_font = pygame.font.SysFont('Arial', 10)
 font_large= pygame.font.SysFont('Arial',30)
 def draw_text(text:str,pos,screen,font=my_font,color=(0,0,0)):
@@ -21,8 +63,8 @@ running = True
 
 background = pygame.Rect(100,100,600,600)
 
-maze_width=8
-maze_height=8
+maze_width=15
+maze_height=15
 
 line_width=600/maze_width
 line_height=600/maze_height
@@ -31,7 +73,7 @@ maze=generate_growing_tree_maze(maze_width,maze_height,choose_index)
 print(maze.display)
 maze_list=maze.grid
 
-directions=[[0 for _ in range(maze_width)] for _ in range (maze_height)]
+directions_list=[[0 for _ in range(maze_width)] for _ in range (maze_height)]
 
 start=(randint(0,maze_width-1),randint(0,maze_height-1))
 end=start
@@ -41,6 +83,16 @@ while end == start:
 def cell_to_pos(cx,cy):
     return (100+(line_width*(cx+0.5)),100+(line_height*(cy+0.5)))
 
+def tCell_to_pos(pos):
+    cx,cy=pos
+    return (100+(line_width*(cx+0.5)),100+(line_height*(cy+0.5)))
+
+solved=False
+sCurX=0
+sCurY=0
+
+path=[]
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -49,21 +101,44 @@ while running:
             running = False
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_s]:
-        # print("s is pressed")
-        start=(randint(0,maze_width-1),randint(0,maze_height-1))
-        end=start
-        while end == start:
-            end=(randint(0,maze_width-1),randint(0,maze_height-1))
+    if pressed[pygame.K_s] or pressed[pygame.K_m]:
+        if pressed[pygame.K_s]:
+            # print("s is pressed")
+            start=(randint(0,maze_width-1),randint(0,maze_height-1))
+            end=start
+            while end == start:
+                end=(randint(0,maze_width-1),randint(0,maze_height-1))
+            solved=False
+            path=[]
+        if pressed[pygame.K_m]:
+            maze=generate_growing_tree_maze(maze_width,maze_height,choose_index)
+            print(maze.display)
+            maze_list=maze.grid
+            solved=False
+            path=[]
     elif pressed[pygame.K_h]:
-        # add maze solving code here
-        pass
+        print("h pressed")
+        if not solved:
+            print("solving maze")
+            solved=True
+            sCurX, sCurY = start    
+            directions_list=[[0 for _ in range(maze_width)] for _ in range (maze_height)]
+            directions_list[sCurY][sCurX]=5
+            path=[]
+            
+            generate_directions(maze,start,end)
+            print(maze.display)
+            path=generate_path_from(end[0],end[1],[],end[0],end[1])
+            print(",".join([str(a) for a in path]))
 
+
+    
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
     draw_text("Press S to randomize start and end points",(10,10),screen,font_large)
     draw_text("Press and hold H to solve maze",(10,40),screen,font_large)
+    draw_text("Press and hold M to re-generate maze",(10,70),screen,font_large)
 
     # draw background
     pygame.draw.rect(screen,(240,240,240),background)
@@ -94,6 +169,11 @@ while running:
     # draw points
     pygame.draw.circle(screen,(0,255,0),cell_to_pos(start[0],start[1]),10)
     pygame.draw.circle(screen,(255,0,0),cell_to_pos(end[0],end[1]),10)
+
+    # draw path
+    if len(path) >=2:
+        for n in range(len(path)-1):
+            pygame.draw.line(screen,(255,0,255),tCell_to_pos(path[n]),tCell_to_pos(path[n+1]))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
